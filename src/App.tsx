53b14db0d495976de984dca759c4afb9471a995e
Jebu Ittiachen <jebu@jebu.net>
2023-03-17 13:53:54 +0530

    some usability text added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { CloseButton, Alert } from 'react-bootstrap';
+import { CloseButton, Alert, Image } from 'react-bootstrap';
 import CheckWebAuthn from './components/CheckWebAuthn';
 
 function App() {
@@ -11,7 +11,9 @@ function App() {
           <CloseButton onClick={ _ => setShow(false)}/>
           <Alert.Heading>Account Abstracted wallet with PassKeys</Alert.Heading>
           <p>
-            This is a demo of an ERC4337 wallet implemented with <Alert.Link href="https://developer.apple.com/passkeys/">PassKeys</Alert.Link>. This should work on any modern browser
+            This is a demo of an <Alert.Link href="https://github.com/eth-infinitism/account-abstraction">ERC4337</Alert.Link> wallet implemented 
+            with PassKeys <Alert.Link href="https://developer.apple.com/passkeys/">Apple</Alert.Link> / 
+            <Alert.Link href="https://developers.google.com/identity/passkeys">Google</Alert.Link>. This should work on any modern browser
             that supports <Alert.Link href="https://www.w3.org/TR/webauthn-2/">WebAuthn</Alert.Link>.
           </p>
           <p>
@@ -23,14 +25,16 @@ function App() {
               <li>Currently contracts are deployed on Polygon - Mumbai and Base - Goerli. Select one of these networks.</li>
               <li>Your conterfactual address is displayed (same on all chains).</li>
               <li>Transfer some testnet MATIC/ETH to this address.</li>
-              <li>Send some native token to another address of yours to deploy the contracts.</li>
+              <li>Send some native token to another address of yours using the <i>Send</i> button to deploy the contracts.</li>
               <li>Once the contracts are deployed you can add additional passkeys to control this wallet.</li>
               <li>To use the wallet on a different device, open this site on that device, enter the wallet <i>Address</i> and click Authenticate.</li>
               <li>Authenticate with any of the passkeys added on the wallet to operate the wallet.</li>
             </ul>            
           </p>
           <p>
-            Gory internal details and the codebase powering this coming soon in a blog post <Alert.Link href="https://www.obvious.technology/blogs">from Obvious</Alert.Link>.
+            Gory internal details and the codebase powering this coming soon in a blog 
+            post <Alert.Link href="https://www.obvious.technology/blogs">from Obvious</Alert.Link>. 
+            I'm available <Alert.Link href="https://twitter.com/jebui">@jebui</Alert.Link> on Twitter.
           </p>
         </Alert>
       <div className="App">
